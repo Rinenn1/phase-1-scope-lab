6ac3dda17af7f6c231e6c471e0584ba2b1719114
Isaac <bennisaac24@gmail.com>
2024-12-06 09:49:02 +0300

    Scope challenge complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    console.log(customerName);
+}
+
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer (){
+    bestCustomer = 'maybe bob'
+}
+//const overwriteBestCustomer = () => bestCustomer = 'maybe bob'
+
+
+const leastFavoriteCustomer = 'surely bob';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'surely not bob';
+}
+
+changeLeastFavoriteCustomer();
+
